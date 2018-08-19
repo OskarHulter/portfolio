0918ba2addfbea4eadcc90602fec91354ccc4ed2
OskarHulter <stresslessness91@gmail.com>
2018-08-19 16:45:45 +0200

    In the process of fixing the gallery. Images added to img.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,5 @@ module.exports = {
     siteMetadata: {
         title: 'Oskar Hulter',
     },
-    plugins: [
-        'gatsby-plugin-react-helmet',
-        'gatsby-transformer-sharp',
-        'gatsby-plugin-sharp',
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                name: 'img',
-                path: `${__dirname}/src/img/`,
-            },
-        },
-    ],
+    plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,12 +1,80 @@
 import React from 'react'
 import img from '../img/img.jpg'
+import Carousel from 'nuka-carousel'
 
-const Gallery = props => (
-    <div className="container-gallery">
-        <div className="img__selected">
-            <img src={img} alt="img" />
-        </div>
-    </div>
-)
+const colors = ['7732bb', '047cc0', '00884b', 'e3bc13', 'db7c00', 'aa231f']
+class Gallery extends React.Component {
+    constructor() {
+        super(...arguments)
+        this.state = {
+            slideIndex: 0,
+            length: 6,
+            wrapAround: true,
+            underlineHeader: false,
+            slidesToShow: 1.0,
+            cellAlign: 'left',
+            transitionMode: 'scroll',
+        }
+    }
+
+    render() {
+        return (
+            <div
+                style={{
+                    width: '100vw',
+                    margin: 'auto',
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}
+            >
+                <Carousel
+                    transitionMode={this.state.transitionMode}
+                    cellAlign={this.state.cellAlign}
+                    slidesToShow={this.state.slidesToShow}
+                    wrapAround={this.state.wrapAround}
+                    slideIndex={this.state.slideIndex}
+                    renderBottomCenterControls={({ currentSlide }) => (
+                        <div className="hej">
+                            My Projects: Slide {currentSlide + 1}
+                        </div>
+                    )}
+                    renderTopCenterControls={({ slideCount }) => (
+                        <div>Slide: {slideCount}</div>
+                    )}
+                >
+                    {colors
+                        .slice(0, this.state.length)
+                        .map((color, index) => (
+                            <img
+                                src={`http://placehold.it/1000x400/${color}/ffffff/&text=slide${index +
+                                    1}`}
+                                key={color}
+                            />
+                        ))}
+                </Carousel>
+                <div className="project-gallery-buttons">
+                    <button onClick={() => this.setState({ slideIndex: 0 })}>
+                        1
+                    </button>
+                    <button onClick={() => this.setState({ slideIndex: 1 })}>
+                        2
+                    </button>
+                    <button onClick={() => this.setState({ slideIndex: 2 })}>
+                        3
+                    </button>
+                    <button onClick={() => this.setState({ slideIndex: 3 })}>
+                        4
+                    </button>
+                    <button onClick={() => this.setState({ slideIndex: 4 })}>
+                        5
+                    </button>
+                    <button onClick={() => this.setState({ slideIndex: 5 })}>
+                        6
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Gallery
