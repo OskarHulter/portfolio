686d787593254c32a5b2f0fc223b378bd961838e
OskarHulter <stresslessness91@gmail.com>
2018-08-25 20:01:05 +0200

    removed nuka & images, added grid-gallery. Some cleanup still needed.

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,5 +1,6 @@
 /* eslint-disable indent */
 import React from 'react'
+import styled from 'styled-components'
 import img1 from '../img/img1.jpg'
 import img2 from '../img/img2.jpg'
 import img3 from '../img/img3.jpg'
@@ -7,111 +8,170 @@ import img4 from '../img/img4.png'
 import img5 from '../img/img5.png'
 import img6 from '../img/img6.png'
 import Carousel from 'nuka-carousel'
+import Lightbox from 'react-images'
 
-const projectImages = [
-    'Fyrisbiografens Vänner',
-    'CMS in PHP',
-    'Grading Backend',
-    'stressless.productions',
-    'festivaltent.shop',
-]
-const images = [img1, img2, img3, img4, img5, img6]
+const GalleryWrapper = styled.section`
+    gallery: {
+        marginright: -2;
+        overflow: 'hidden';
+        /*
+		@media (min-width: 500px): {
+			marginRight: -gutter.large;
+		}
+        */
+    }
+
+    /* anchor */
+    thumbnail: {
+        boxsizing: 'border-box';
+        display: 'block';
+        float: 'left';
+        line-height: 0;
+        padding-right: 2;
+        padding-bottom: 2;
+        overflow: 'hidden';
+        /*
+		@media (min-width: 500px): {
+			paddingRight: gutter.large;
+			paddingBottom: gutter.large;
+		}
+        */
+    }
+
+    ,
+
+	/*orientation */
+	landscape: {
+        width: '30%';
+    }
+    square: {
+        padding-bottom: 0;
+        width: '40%';
+
+        /*
+		@media (min-width: 500px): {
+			paddingBottom: 0;
+		}
+        */
+    }
+
+    /*actual <img />*/
+    source: {
+        border: 0;
+        display: 'block';
+        height: 'auto';
+        maxwidth: '100%';
+        width: 'auto';
+    }
+`
 class Gallery extends React.Component {
     constructor() {
-        super(...arguments)
+        super()
         this.state = {
-            slideIndex: 0,
-            length: 5,
-            wrapAround: true,
-            underlineHeader: false,
-            slidesToShow: 1.0,
-            cellAlign: 'left',
-            transitionMode: 'scroll',
+            lightboxIsOpen: 'False',
+            currentImage: 0,
         }
+
+        this.closeLightbox = this.closeLightbox.bind(this)
+        this.gotoNext = this.gotoNext.bind(this)
+        this.gotoPrevious = this.gotoPrevious.bind(this)
+        this.gotoImage = this.gotoImage.bind(this)
+        this.handleClickImage = this.handleClickImage.bind(this)
+        this.openLightbox = this.openLightbox.bind(this)
     }
-    getImage(currentSlide) {
-        switch (currentSlide) {
-            case 0:
-                return img1
-            case 1:
-                return img2
-            case 2:
-                return img3
-            case 3:
-                return img4
-            case 4:
-                return img5
-            default:
-                return img6
-        }
+
+    openLightbox(index, event) {
+        event.preventDefault()
+        this.setState({
+            currentImage: index,
+            lightboxIsOpen: true,
+        })
+    }
+    closeLightbox() {
+        this.setState({
+            currentImage: 0,
+            lightboxIsOpen: false,
+        })
+    }
+    gotoPrevious() {
+        this.setState({
+            currentImage: this.state.currentImage - 1,
+        })
+    }
+    gotoNext() {
+        this.setState({
+            currentImage: this.state.currentImage + 1,
+        })
+    }
+    gotoImage(index) {
+        this.setState({
+            currentImage: index,
+        })
+    }
+    handleClickImage() {
+        if (this.state.currentImage === this.props.images.length - 1) return
+
+        this.gotoNext()
+    }
+    renderGallery() {
+        const { images } = this.props
+
+        if (!images) return
+
+        const gallery = images.filter(i => i.useForDemo).map((obj, i) => {
+            return (
+                <a
+                    href={obj.src}
+                    className={(thumbnail, obj.orientation)}
+                    key={i}
+                    onClick={e => this.openLightbox(i, e)}
+                >
+                    <img src={obj.thumbnail} className={source} />
+                </a>
+            )
+        })
+
+        return <div className={gallery}>{gallery}</div>
     }
     render() {
         return (
-            <div className="gallery-carousel">
-                <Carousel
-                    transitionMode={this.state.transitionMode}
-                    cellAlign={this.state.cellAlign}
-                    slidesToShow={this.state.slidesToShow}
-                    wrapAround={this.state.wrapAround}
-                    slideIndex={this.state.slideIndex}
-                    /* 
-                    renderBottomCenterControls={({ description, currentSlide }) => (
-                        <div className="project-description-text">
-                            My Projects: Slide {currentSlide + 1}
-                        </div>
-                    )}
-                    */
-                    renderBottomCenterControls={({ currentSlide }) => (
-                        <div className="hej">
-                            My Projects: Slide {currentSlide + 1}
-                        </div>
-                    )}
-                    renderTopCenterControls={({ slideCount }) => (
-                        <div>Slide: {slideCount}</div>
-                    )}
-                >
-                    {images
-                        .slice(0, this.state.length)
-                        .map((image, index) => (
-                            <img src={this.getImage(index)} key={index} />
-                        ))}
-                </Carousel>
-                <div className="project-gallery-buttons">
-                    <button onClick={() => this.setState({ slideIndex: 0 })}>
-                        1
-                    </button>
-                    <button onClick={() => this.setState({ slideIndex: 1 })}>
-                        2
-                    </button>
-                    <button onClick={() => this.setState({ slideIndex: 2 })}>
-                        3
-                    </button>
-                    <button onClick={() => this.setState({ slideIndex: 3 })}>
-                        4
-                    </button>
-                    <button onClick={() => this.setState({ slideIndex: 4 })}>
-                        5
-                    </button>
-                </div>
-            </div>
+            <GalleryWrapper>
+                {this.props.heading && <h2>{this.props.heading}</h2>}
+                {this.props.subheading && <p>{this.props.subheading}</p>}
+                {this.renderGallery()}
+                <Lightbox
+                    currentImage={this.state.currentImage}
+                    images={this.props.images}
+                    isOpen={this.state.lightboxIsOpen}
+                    onClickImage={this.handleClickImage}
+                    onClickNext={this.gotoNext}
+                    onClickPrev={this.gotoPrevious}
+                    onClickThumbnail={this.gotoImage}
+                    onClose={this.closeLightbox}
+                    preventScroll={this.props.preventScroll}
+                    showThumbnails={this.props.showThumbnails}
+                    spinner={this.props.spinner}
+                    spinnerColor={this.props.spinnerColor}
+                    spinnerSize={this.props.spinnerSize}
+                    theme={this.props.theme}
+                />
+            </GalleryWrapper>
         )
     }
 }
-
-export default Gallery
+Gallery.displayName = 'Gallery'
 /*
-! TIPS FOR GETTING STARTED WITH RESPONSIVE IMAGES
-
-The background-size property is ie>=9 only, but if that is fine with you, you can use a div with background-image and set background-size: contain:
-
-div.image{
-    background-image: url("your/url/here");
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center;
+Gallery.propTypes = {
+    heading: PropTypes.string,
+    images: PropTypes.array,
+    showThumbnails: PropTypes.bool,
+    subheading: PropTypes.string,
 }
-Now you can just set your div size to whatever you want and not only will the image keep its aspect ratio it will also be centralized both vertically and horizontally within the div. Just don't forget to set the sizes on the css since divs don't have the width/height attribute on the tag itself.
-
-This approach is different than setecs answer, using this the image area will be constant and defined by you (leaving empty spaces either horizontally or vertically depending on the div size and image aspect ratio), while setecs answer will get you a box that exactly the size of the scaled image (without empty spaces).
-hmm, function orderTotal  
 */
+/*
+const gutter = {
+    small: 2,
+    large: 4,
+}
+*/
+export default Gallery
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,41 @@
 import React from 'react'
-import Gallery from '../components/gallery'
+import img1 from '../img/img1.jpg'
+import Gallery from 'react-grid-gallery'
+
+const IMAGES = [
+    {
+        src: img1,
+        thumbnail: img1,
+        thumbnailWidth: 320,
+        thumbnailHeight: 174,
+        isSelected: true,
+        caption: 'A responsive website built by Me for abc with this and this',
+    },
+    {
+        src: 'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
+        thumbnail:
+            'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg',
+        thumbnailWidth: 320,
+        thumbnailHeight: 212,
+        tags: [
+            { value: 'Ocean', title: 'Ocean' },
+            { value: 'People', title: 'People' },
+        ],
+        caption: 'Boats (Jeshu John - designerspics.com)',
+    },
+
+    {
+        src: 'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg',
+        thumbnail:
+            'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg',
+        thumbnailWidth: 320,
+        thumbnailHeight: 212,
+    },
+]
 
 const ProjectPage = () => (
     <section className="project-gallery">
-        <Gallery />
+        <Gallery images={IMAGES} />
     </section>
 )
 
