57dfd1a033028e102b74a6e05995165d1348f269
OskarHulter <stresslessness91@gmail.com>
2018-08-25 17:10:37 +0200

    added first styled component, will continue & clean up css

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,19 +2,25 @@ import React from 'react'
 import styled from 'styled-components'
 import SocialButtons from '../components/socialbuttons'
 
-const contactPage = styled.div`
-    display: flex;
+const blue = ''
+
+const ContactWrapper = styled.div`
+    display: grid;
+    width: 100vw;
+    height: 100vh;
+    color: #25008b;
+    background-color: #2ea1ab;
 `
 
 const ContactPage = () => (
-    <div className="contact-page">
+    <ContactWrapper>
         <a href="mailto:oskarhulter@gmail.com">
             <div className="page-subtitle">
                 <p>oskarhulter@gmail.com</p>
             </div>
         </a>
         <SocialButtons />
-    </div>
+    </ContactWrapper>
 )
 
 export default ContactPage
