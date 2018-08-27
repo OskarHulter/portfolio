9d476c422c8a07e7bc780f98c25673ab85baf5df
OskarHulter <stresslessness91@gmail.com>
2018-08-28 00:13:03 +0200

    added fa icons

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import { FaReact } from 'react-icons/fa'
+import { FaHtml5 } from 'react-icons/fa'
+import { FaCss3Alt } from 'react-icons/fa'
+import { FaJsSquare } from 'react-icons/fa'
 
 const AboutWrapper = styled.section`
     display: grid;
@@ -7,34 +11,41 @@ const AboutWrapper = styled.section`
     height: 100vh;
     width: 100vw;
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: 1fr 5fr;
+    line-height: 1.5;
 `
 
 const TextContent = styled.div`
-    font-size: 1.2em;
-    line-height: 1.7;
     margin: 1.5em 0;
 `
-
+const SkillText = styled.div`
+    margin: 1.5em 0;
+    font-size: 0.5em;
+`
 const AboutPage = () => (
     <AboutWrapper>
-        <h1 className="title-main">programmer:</h1>
+        <div>
+            <FaHtml5 size={17} />
+            <FaCss3Alt size={17} />
+            <FaJsSquare size={17} />
+            <FaReact size={17} />
+        </div>
+        <SkillText>
+            <h1 className="title-main">programmer:</h1>
+            *Passion for modern web. Specializing in JavaScript but enjoys
+            learning other technologies. Java C PHP Methodologies & Best
+            practices: Agile LEAN Kanban Mobile-first Bachelor's in Systems
+            Science (LTU, 2018).
+        </SkillText>
+        <SkillText>html5 css3 javascript react redux gatsby</SkillText>
         <TextContent>
-            *Bachelor's Degree in Systems Science (LTU, 2018). *Passion for
-            building modern web experiences. *Specialized in JavaScript *enjoys
-            learning other technologies. Previous experience: Java C PHP
-            Methodologies & Best practices: Agile LEAN Kanban Mobile-first
-        </TextContent>
-        <h1 className="title">person:</h1>
-        <TextContent>
+            <h1 className="title">person:</h1>
             My personality type is INTP (big 5 psychological test). My other
-            great passions are music, strategy games & hiking.
-        </TextContent>
-        <TextContent>
-            My favorite topics include modern javascript, testing, functional &
-            declarative programmering. Some of my favorite TOOLS, LEARNING
-            RESOURCES, HOBBIES, COLLECTIBLES
+            great passions are music, strategy games & hiking. My favorite
+            topics include modern javascript, testing, functional & declarative
+            programmering. Some of my favorite TOOLS, LEARNING RESOURCES,
+            HOBBIES, COLLECTIBLES
         </TextContent>
     </AboutWrapper>
 )
