54b7f2cc78e657ffa099156393e2fa7e148dc124
OskarHulter <stresslessness91@gmail.com>
2018-09-02 18:59:23 +0200

    added .. again...

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import UpButton from 'components/UpButton'
+import UpButton from '../components/UpButton'
 import { FaReact } from 'react-icons/fa'
 import { FaHtml5 } from 'react-icons/fa'
 import { FaCss3Alt } from 'react-icons/fa'
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
-import Layout from 'components/Layout'
+import Layout from '../components/Layout'
 import Landing from './Landing'
 import AboutPage from './About'
 import ProjectGallery from './Gallery'
-import Footer from 'components/Footer'
+import Footer from '../components/Footer'
 
 const GlobalWrapper = styled.div`
     display: flex;
