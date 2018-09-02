c99743e396b854b6ebf69df9a6f0ea19ef6e97cd
OskarHulter <stresslessness91@gmail.com>
2018-09-02 17:00:17 +0200

    changed to ./ for most imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 import Layout from 'components/Layout'
-import Landing from 'Landing'
-import AboutPage from 'About'
-import ProjectGallery from 'Gallery'
+import Landing from './Landing'
+import AboutPage from './About'
+import ProjectGallery from './Gallery'
 import Footer from 'components/Footer'
 
 const GlobalWrapper = styled.div`
